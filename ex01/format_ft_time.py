4e0c64369b888fb71515b8d3f7a416b1f0a1c032
genouf <gabriel.enouf@yahoo.fr>
2024-09-08 12:00:29 +0200

    remove useless spaces

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 actualTime = datetime.now()
 
-print("Seconds since January 1, 1970: ", format(actualTime.timestamp(), ","), "or", format(actualTime.timestamp(), ".2e"), "in scientific notation")
+print("Seconds since January 1, 1970:", format(actualTime.timestamp(), ","), "or", format(actualTime.timestamp(), ".2e"), "in scientific notation")
 print(actualTime.strftime("%b %d %Y"))
--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -2,18 +2,18 @@ def all_thing_is_obj(object: any) -> int:
     typeObject = type(object)
 
     if typeObject == list:
-        print("List : ", typeObject)
+        print("List :", typeObject)
     elif typeObject == tuple:
-        print("Tuple : ", typeObject)
+        print("Tuple :", typeObject)
     elif typeObject == set:
-        print("Set : ", typeObject)
+        print("Set :", typeObject)
     elif typeObject == dict:
-        print("Dict : ", typeObject)
+        print("Dict :", typeObject)
     elif typeObject == str:
         if object == "Brian":
-            print("Brian is in the kitchen : ", typeObject)
+            print("Brian is in the kitchen :", typeObject)
         elif object == "Toto":
-            print("Toto is in the kitchen : ", typeObject)
+            print("Toto is in the kitchen :", typeObject)
     else:
         print("Type not found")
 
